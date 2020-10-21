4a522cb1ce6e1d23ee2b2e9ad13c8609e86fc139
cris12gm <cris12gm@gmail.com>
2020-10-21 18:03:08 +0200

    dc

--- updateDatabaseContent.py
+++ updateDatabaseContent.py
@@ -7,7 +7,27 @@ fileContent = open(sys.argv[1],'r')
 
 MongoEngine().set_database_name('hg38')
 
+d = {}
 for line in fileContent:
-    print (line)
+    line = line.strip().split("\t")
+    d = {
+        '_id':line[0],
+        'specie': line[1],
+        'assembly': line[2],
+        'individual': line[3],
+        'sample': line[4],
+        'context': line[5],
+        'sex': line[6],
+        'age': line[7],
+        'physiopatological_status': line[8],
+        'description': line[9],
+        'reference': line[10],
+        'biosample': line[11],
+        'srx':line[12],
+        'dump5': line[13]
+    }
+
+print (d)
+
 #Content().insert({'sample':sampleName, 'methylationContext' : "CG"})
 
