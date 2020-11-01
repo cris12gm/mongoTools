096d296b4de17b72dc8db3cd3b3b0122bde4495b
cris12gm <cris12gm@gmail.com>
2020-11-01 22:32:26 +0100

    errorFixed

--- createJson.py
+++ createJson.py
@@ -34,7 +34,9 @@ for line in fileMeth:
 
 print ("End")
 
+chromsAllowed = {"chr1":"","chr2":"","chr3":"","chr4":"","chr5":"","chr6":"","chr7":"","chr8":"","chr9":"","chr10":"","chr11":"","chr12":"","chr13":"","chr14":"","chr15":"","chr16":"","chr17":"","chr18":"","chr19":"","chr20":"","chr21":"","chr22":"","chrM":"","chrX":"","chrY":"","chrEBV":""]
 for chrom in valuesByChrom:
-    out = sys.argv[1]+"_"+chrom+".json"    
-    with open(out, 'w') as outfile:
-        json.dump(valuesByChrom[chrom], outfile)
+    if chrom in chromsAllowed:
+        out = sys.argv[1]+"_"+chrom+".json"    
+        with open(out, 'w') as outfile:
+            json.dump(valuesByChrom[chrom], outfile)
