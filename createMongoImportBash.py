790e761d7a97a779546fc63ab538fdc4cfacd703
cris12gm <cris12gm@gmail.com>
2020-11-02 08:57:02 +0100

    added createMongoImportBash

--- createMongoImportBash.py
+++ createMongoImportBash.py
@@ -3,7 +3,7 @@ import json
 import os.path
 
 fileSamples = open(sys.argv[1],'r')
-fileBatch = open(sys.argv[2],'r')
+fileBatch = open(sys.argv[2],'a')
 
 chromsAllowed = {"chr1":"","chr2":"","chr3":"","chr4":"","chr5":"","chr6":"","chr7":"","chr8":"","chr9":"","chr10":"","chr11":"","chr12":"","chr13":"","chr14":"","chr15":"","chr16":"","chr17":"","chr18":"","chr19":"","chr20":"","chr21":"","chr22":"","chrM":"","chrX":"","chrY":"","chrEBV":""}
 for line in fileSamples:
